af66e61d96648f58b64b272a93b88fdd41b81512
wilfredomateo <wmateo86@gmail.com>
2022-11-23 13:51:56 -0600

    added functionality

--- rock-paper-scissor.js
+++ rock-paper-scissor.js
@@ -1,1 +1,20 @@
 const fs = require('fs');
+const http = require('http');
+
+const server = http.createServer(request, response) => {
+  
+  const url = new URL (request.url, 'http://${request.headers.host}');
+
+  switch(url.pathname) {
+    case '/':
+      if (request.method === 'GET') {
+        const name = url.searchParams.get('name');
+        console.log(name);
+      }
+      break;
+    default:
+      request.end('ok');
+  }
+
+
+}
